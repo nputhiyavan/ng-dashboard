909e8221e095d8bcd97d21ef51ba54c3f4a0c66f
Puthiyavan <NPuthiyavan@ashleyfurniture.com>
2020-05-29 20:22:38 +0530

    Dashboard initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    DashboardHomeComponent
   ],
   imports: [
     BrowserModule
